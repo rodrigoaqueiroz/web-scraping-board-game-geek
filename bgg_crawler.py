296f77b875450fa2f63e271fc7fc231ff984baa4
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-04-18 17:46:43 -0300

    feat: insert bg details in DB

--- bgg_crawler.py
+++ bgg_crawler.py
@@ -1,11 +1,11 @@
 import requests, bs4
 import time
 from query import insert_data
+import json
 
 
 URL_BASE = 'https://boardgamegeek.com/browse/boardgame/page/1'
 bg_names = []
-bg_links = []
 
 
 def fetch_url(url):
@@ -23,14 +23,33 @@ def get_boardgame_info(content):
     cont = 1
     for name in content.find_all('tr', id="row_"):
         bg_names.append(
-            (name.find('div',  id = f'results_objectname{cont}').text.replace('\n', '')[:-6],
-            name.find('div',  id = f'results_objectname{cont}').text.replace('\n', '')[-5:-1],
-            name.find_all('td',  class_ = 'collection_bggrating')[-1].text.split().pop())
+            (name.find('div', id = f'results_objectname{cont}').text.replace('\n', '')[:-6],
+            name.find('div', id = f'results_objectname{cont}').text.replace('\n', '')[-5:-1],
+            name.find_all('td', class_ = 'collection_bggrating')[-1].text.split().pop())
         )
-        details = name.find('a',  class_ = 'primary')['href']
+        details = name.find('a', class_ = 'primary')['href']
         page = f'https://boardgamegeek.com{details}'
-        bg_links.append(page)
-        insert_data(bg_names[len(bg_names)-1][0], bg_names[len(bg_names)-1][1], bg_names[len(bg_names)-1][2])
+        response = requests.get(page, timeout=10)
+        time.sleep(1)
+        soup = bs4.BeautifulSoup(response.content, 'html.parser')
+        scripts = soup.find_all('script', attrs = {'type': None })[2]
+        data_json = ''
+        for l in scripts.text.split('\n'):
+            if l.strip().startswith('GEEK.geekitemPreload'):
+                data_json = json.loads(l.strip()[23:-1])
+                data = data_json['item']['polls']
+                best_for = data['userplayers']['best']
+                if len(best_for) < 1:
+                    best_for = 0
+                else:
+                    best_for = data['userplayers']['best'][0]['max']
+                weigth = data['boardgameweight']['averageweight']
+                insert_data(
+                    bg_names[len(bg_names)-1][0], 
+                    bg_names[len(bg_names)-1][1], 
+                    bg_names[len(bg_names)-1][2],
+                    best_for,
+                    weigth)
         cont += 1
     return bg_names
         
@@ -41,3 +60,5 @@ def next_page():
         content = fetch_url(url)
         get_boardgame_info(content)
     return bg_names
+        
+next_page()
--- query.py
+++ query.py
@@ -1,22 +1,20 @@
 from datetime import date
-from tkinter import INSERT
 import mysql.connector
+
 
 db_connection = mysql.connector.connect(host="localhost", user="root", passwd="", database="web_scrapping_bgg")
 cursor = db_connection.cursor()
 
-# sql = "INSERT INTO user (name, cpf) VALUES ('adsa', '000s.000.000.00')"
-# cursor.execute(sql)
 
-
-def insert_data(name, year, popularity):    
-  cursor = db_connection.cursor()
-  sql = "INSERT INTO bg (bg_name, year, popularity) VALUES (%s, %s, %s)"
-  values = (name, year, popularity)
-  cursor.execute(sql, values)
-  db_connection.commit()
+def insert_data(name, year, popularity, best_for, weigth):  
+  try:  
+    cursor = db_connection.cursor()
+    sql = "INSERT INTO bg (bg_name, year, popularity, best_for, weigth) VALUES (%s, %s, %s, %s, %s)"
+    values = (name, year, popularity, best_for, weigth)
+    cursor.execute(sql, values)
+    db_connection.commit()
+  except mysql.connector.Error as err:
+    print(f"Something went wrong: {err}")
 
 
 cursor.close()
-# db_connection.commit()
-# db_connection.close()
